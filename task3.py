9c4baecc764307087287e8dadd7576fe38245084
Artem Maklashev <artem@maklashev.ru>
2022-12-20 13:10:03 +0400

    task3 fixed

--- task3.py
+++ task3.py
@@ -4,7 +4,7 @@
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 import os
-from random import uniform
+from random import uniform, randint
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
@@ -21,25 +21,28 @@ def GetNumber(message):
 
 def InitList(number):
     my_list = []
+    index = randint(0,4)
     for i in range (number):
-        my_list.append(round(uniform(0, 10), 2))
+        my_list.append(round(uniform(0, 10), index))
     return my_list
 
 def TakeDifference(my_list):
     dif = 0
     dif_list = []
     for i in range(len(my_list)):
-        dif_list.append(round(my_list[i]%1, 2))
-        if i == 0:
-            max = dif_list[i]
-            min = dif_list[i]
-        else:
-            if dif_list[i]>max:
-                max = dif_list[i]
-            if dif_list[i] < min:
-                min = dif_list[i]
+        if my_list[i]%1 > 0:
+            dif_list.append(round(my_list[i]%1,3))
+    for j in range(len(dif_list)):        
+            if j == 0:
+                max = dif_list[j]
+                min = dif_list[j]
+            else:
+                if dif_list[j]>max:
+                    max = dif_list[j]
+                if dif_list[j] < min:
+                    min = dif_list[j]
     print(f'Дробная часть списка: {dif_list}')
-    dif = round(max-min, 2)
+    dif = round(max-min, 3)
     return dif
 
         
